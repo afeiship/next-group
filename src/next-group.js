f7ac1c9453ef0897258b75028d98b95b2be1ddf9
afeiship <afeiship@gmail.com>
2018-12-15 22:50:50 +0800

    refactor:

--- src/next-group.js
+++ src/next-group.js
@@ -3,65 +3,78 @@
   var nx = global.nx || require('next-js-core2');
   var FUNC = 'function';
 
-  /*
-  
-  var groups = [
-    {
-      id:'g1',
-      items:[ data1, data2 ]
-    },
-    {
-      id:'g2',
-      items:[ data3, data4 ]
-    }
-  ]
-
-  mutexGroup
-  next-mutext-group
-  */
-
   var NxGroup = nx.declare('nx.Group', {
-    propteries: {
+    properties: {
       items: {
         get: function() {
           var result = [];
-          return this._groups.reduce(function(group1, group2) {
-            return result.concat(group1.items, group2.items);
+          this._groups.forEach(function(group) {
+            result = result.concat(group.items || []);
           });
+          return result;
         }
-      },
-      maps: {
-        get: function() {}
       },
       groups: {
         get: function() {
           return this._groups;
         }
       },
-      length: {
+      maps: {
         get: function() {
-          return this._groups.length;
+          var itemMap = {};
+          var groupMap = {};
+          var self = this;
+          nx.each(this._groups, function(_, group) {
+            groupMap[group.id] = group.items;
+            nx.each(group.items, function(__, item) {
+              itemMap[self.tid(item)] = item;
+            });
+          });
+
+          return {
+            item: itemMap,
+            group: groupMap
+          };
         }
       }
     },
     methods: {
-      init: function(inData, inId) {
-        this.data = inData;
-        this.id = inId;
+      /**
+       * 接受两个参数：
+       * inGroupId: 这个是 group 的唯一标识，可能是一个 any/function
+       * inItemId: 这个是每个数据  item 的唯一标识，可能是一个 any/function
+       * @param {*} inGroupId
+       * @param {*} inItemId
+       */
+      init: function(inGroupId, inItemId) {
+        this.groupId = inGroupId;
+        this.itemId = inItemId;
         this._groups = [];
       },
-      get: function(inId) {
-        var index = this.index(inId);
-        return index === -1 ? null : this._groups[index];
+      id: function(inKey, inItem) {
+        return typeof this[inKey] === FUNC ? this[inKey](inItem) : inItem[this[inKey]];
       },
-      getId: function(inItem) {
-        return typeof this.id === FUNC ? this.id(inItem) : inItem[this.id];
+      gid: function(inItem) {
+        return this.id('groupId', inItem);
       },
-      indexOf: function(inId) {
-        var index = -1;
+      tid: function(inItem) {
+        return this.id('itemId', inItem);
+      },
+      indexOfGroup: function(inItem) {
+        var gid = this.gid(inItem);
         for (var i = 0; i < this._groups.length; i++) {
           var group = this._groups[i];
-          if (group.id === inId) {
+          if (group.id === gid) {
+            return i;
+          }
+        }
+        return -1;
+      },
+      indexOf: function(inKey, inItems, inItem) {
+        var index = -1;
+        for (var i = 0; i < inItems.length; i++) {
+          var item = inItems[i];
+          if (this[inKey](inItem) === this[inKey](item)) {
             index = i;
             break;
           }
@@ -69,25 +82,29 @@
         return index;
       },
       contains: function(inItem) {
-        return this.items.indexOf(inItem) > -1;
+        return this.indexOf('tid', this.items, inItem) > -1;
       },
       add: function(inItem) {
-        var id = this.getId(inItem);
-        var gIndex = this.indexOf(id);
-        if (gIndex === -1) {
-          this._groups.push({ id: id, items: [inItem] });
-        } else {
-          this._groups[gIndex].items.push(inItem);
+        if (!this.contains(inItem)) {
+          var gid = this.gid(inItem);
+          var gIdx = this.indexOfGroup(inItem);
+          if (gIdx !== -1) {
+            this._groups[gIdx].items.push(inItem);
+          } else {
+            this._groups.push({ id: gid, items: [inItem] });
+          }
         }
       },
       remove: function(inItem) {
-        var id = this.getId(inItem);
-        var gIndex = this.indexOf(id);
-        if (gIndex !== -1) {
-          var iIndex = this._groups[gIndex].items.indexOf(inItem);
-          this._groups[gIndex].items.splice(iIndex, 1);
-          if (this._groups[gIndex].items.length === 0) {
-            this._groups.splice(gIndex, 1);
+        var gIdx = this.indexOfGroup(inItem);
+        if (gIdx !== -1) {
+          var group = this._groups[gIdx];
+          var tIdx = this.indexOf('tid', group.items, inItem);
+          if (tIdx !== -1) {
+            group.items.splice(tIdx, 1);
+            if (!group.items.length) {
+              this._groups.splice(gIdx, 1);
+            }
           }
         }
       },
--- test/test.js
+++ test/test.js
@@ -1,21 +1,32 @@
-(function () {
-  var assert = require('assert');
-  var nx = require('next-js-core2');
-  var NxGroup = require('../src/next-group');
+var nx = require('next-js-core2');
+var NxGroup = require('../src/next-group');
 
-  describe('NxGroup.methods', function () {
-    it('json/urlencoded', function () {
-      var data = {
-        key: 1,
-        value: 2
-      };
-      // console.log(rs1, rs2);
-
-      // assert.equal(rs1, '{"key":1,"value":2}');
-      // assert.equal(rs2, 'key=1&value=2');
-
-    });
+test('test data group', () => {
+  var dataGroup = new NxGroup('sex', (item) => {
+    return 'tid_' + item.name + '_' + item.sex;
   });
+  dataGroup.add({ name: 'a', sex: 'male' });
+  dataGroup.add({ name: 'b', sex: 'male' });
+  dataGroup.add({ name: 'c', sex: 'female' });
+  dataGroup.add({ name: 'd', sex: 'female' });
+  dataGroup.add({ name: 'e', sex: 'male' });
+  dataGroup.add({ name: 'f', sex: 'female' });
 
+  // console.log(dataGroup);
+  expect(dataGroup.items.length).toBe(6);
+  expect(dataGroup.groups.length).toBe(2);
 
-}());
+  // remove:
+  dataGroup.remove({ name: 'a', sex: 'male' });
+  dataGroup.remove({ name: 'b', sex: 'male' });
+  dataGroup.remove({ name: 'e', sex: 'male' });
+  // expect(dataGroup.items.length).toBe(5);
+  // expect(dataGroup.groups.length).toBe(2);
+
+  // dataGroup.remove({ name: 'e', sex: 'female' });
+
+  console.log(dataGroup);
+
+  // expect(dataGroup.items.length).toBe(5);
+  // expect(dataGroup.groups.length).toBe(2);
+});
